aad0e8f21dd942168efb84d9a6a0a7e80151af18
Dinara L <dinaralatypova16@mail.ru>
2023-02-12 20:40:04 +0500

    task2: first solution

--- tasks/task2/transform.hpp
+++ tasks/task2/transform.hpp
@@ -1,4 +1,32 @@
+#include <exception>
 template <class T>
-void TransformIf(T* begin, T* end, bool (*p)(const T&), void (*f)(T&)) {
-    // Your code goes here
+void TransformIf(T *begin, T *end, bool (*p)(const T &), void (*f)(T &)) {
+  std::size_t size = end - begin;
+  T *copy = new T[size];
+  for (std::size_t i = 0; begin + i != end; ++i) {
+    try {
+      if (p(begin[i])) {
+        try {
+          copy[i] = begin[i];
+        } catch (...) {
+        }
+        try {
+          f(begin[i]);
+        } catch (...) {
+          throw;
+        }
+      }
+    } catch (...) {
+      try {
+        for (std::size_t j = 0; j <= i; ++j) {
+          begin[j] = copy[j];
+        }
+      } catch (...) {
+        delete[] copy;
+        throw;
+      }
+      delete[] copy;
+      throw;
+    }
+  }
 }
