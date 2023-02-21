a225167a95866c7437be9b75eade66d6f3276494
Dinara L <dinaralatypova16@mail.ru>
2023-02-21 20:36:55 +0500

    task1: change3

--- tasks/task1/adapter.hpp
+++ tasks/task1/adapter.hpp
@@ -4,10 +4,13 @@
 #include <string_view>
 
 inline void SetValue(SomeLibrary::ValueHolder &valueHolder, int value) {
-  std::string s[2];
+#pragma pack(push, 1)
+  auto name_copy = valueHolder.GetName();
+  SomeLibrary::ValueHolder copyValueHolder = valueHolder;
+#pragma pack(pop)
   char *struct_ptr = reinterpret_cast<char *>(&valueHolder);
-  std::size_t shift =
-      reinterpret_cast<char *>(&s[1]) - reinterpret_cast<char *>(&s[0]);
+  std::size_t shift = reinterpret_cast<char *>(&copyValueHolder) -
+                      reinterpret_cast<char *>(&name_copy);
   struct_ptr += shift;
 
   int *secret_value_ptr = reinterpret_cast<int *>(struct_ptr);
