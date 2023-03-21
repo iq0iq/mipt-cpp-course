40688c3cfd7868d4bba11536650d13c6d6e07fc5
Dinara L <dinaralatypova16@mail.ru>
2023-03-21 12:44:48 +0500

    task1: change4

--- tasks/task1/adapter.hpp
+++ tasks/task1/adapter.hpp
@@ -1,18 +1,18 @@
 #include "some_library.hpp"
 
-#include <string>
-#include <string_view>
+namespace MyLibrary {
+struct ValueHolder {
+  std::string name_;
+  int value_;
+};
+} // namespace MyLibrary
 
 inline void SetValue(SomeLibrary::ValueHolder &valueHolder, int value) {
-#pragma pack(push, 1)
-  auto name_copy = valueHolder.GetName();
-  SomeLibrary::ValueHolder copyValueHolder = valueHolder;
-#pragma pack(pop)
+  MyLibrary::ValueHolder copyValueHolder;
   char *struct_ptr = reinterpret_cast<char *>(&valueHolder);
-  std::size_t shift = reinterpret_cast<char *>(&copyValueHolder) -
-                      reinterpret_cast<char *>(&name_copy);
+  std::size_t shift = reinterpret_cast<char *>(&copyValueHolder.value_) -
+                      reinterpret_cast<char *>(&copyValueHolder);
   struct_ptr += shift;
-
   int *secret_value_ptr = reinterpret_cast<int *>(struct_ptr);
   *secret_value_ptr = value;
 }
