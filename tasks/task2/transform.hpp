e6139aa09e3901a237df5bfbb41f2c7c645f5666
Dinara L <dinaralatypova16@mail.ru>
2023-03-21 12:29:10 +0500

    task2: change2

--- tasks/task2/transform.hpp
+++ tasks/task2/transform.hpp
@@ -1,26 +1,29 @@
 #include <exception>
+#include <iostream>
 #include <vector>
 template <class T>
 void TransformIf(T *begin, T *end, bool (*p)(const T &), void (*f)(T &)) {
   std::size_t size = end - begin;
   std::vector<T> copy(size);
+  std::vector<bool> flag(size, false);
   for (std::size_t i = 0; begin + i != end; ++i) {
-    try {
-      copy[i] = begin[i];
-    } catch (...) {
-    }
     try {
       if (p(begin[i])) {
         try {
-          f(begin[i]);
+          copy[i] = begin[i];
+          flag[i] = true;
         } catch (...) {
-          begin[i] = copy[i];
-          throw;
+          flag[i] = false;
         }
+        f(begin[i]);
       }
     } catch (...) {
-      for (std::size_t j = 0; j < i; ++j) {
-        begin[j] = copy[j];
+      for (std::size_t j = 0; j <= i; ++j) {
+        try {
+          if (flag[j])
+            begin[j] = copy[j];
+        } catch (...) {
+        }
       }
       throw;
     }
