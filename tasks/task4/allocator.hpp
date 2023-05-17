5cf051a2c1bf0ca5c7e726374269a44af63f6da2
Dinara L <dinaralatypova16@mail.ru>
2023-05-17 23:03:38 +0500

    task4: init solution

--- tasks/task4/allocator.hpp
+++ tasks/task4/allocator.hpp
@@ -1,32 +1,49 @@
 #pragma once
 
-// When chunk is free 'next' contains pointer to a next free chunk
-// When it's allocated, this space is used by user
 struct Chunk {
-    Chunk* next;
+  Chunk *next;
 };
 
 class PoolAllocator {
 public:
-    PoolAllocator(size_t chunksPerBlock) : chunksPerBlock_(chunksPerBlock), alloc_(nullptr) { }
+  PoolAllocator(size_t chunksPerBlock)
+      : chunksPerBlock_(chunksPerBlock), alloc_(nullptr) {}
 
-    void* allocate(size_t size) {
-        if (alloc_ == nullptr) {
-            alloc_ = allocateBlock(size);
-        }
-        // your code goes here
+  void *allocate(size_t size) {
+    if (alloc_ == nullptr) {
+      alloc_ = allocateBlock(size);
     }
+    Chunk *copy = alloc_;
+    alloc_ = alloc_->next;
+    return copy;
+  }
 
-    void deallocate(void* chunk, size_t size) {
-        // your code goes here
+  void deallocate(void *chunk, size_t size) {
+    ::operator delete(chunk);
+    if (alloc_ == nullptr) {
+      alloc_ = reinterpret_cast<Chunk *>(chunk);
+      return;
     }
+    Chunk *next_ = alloc_->next;
+    alloc_->next = reinterpret_cast<Chunk *>(chunk);
+    if (next_ != nullptr)
+      alloc_->next->next = next_;
+  }
 
 private:
-    size_t chunksPerBlock_;
-    Chunk* alloc_;
+  size_t chunksPerBlock_;
+  Chunk *alloc_;
 
 private:
-    Chunk* allocateBlock(size_t chunkSize) {
-        // your code goes here
+  Chunk *allocateBlock(size_t chunkSize) {
+    char *start =
+        reinterpret_cast<char *>(::operator new(chunkSize *chunksPerBlock_));
+    Chunk *current = new (start) Chunk();
+    for (size_t i = 1; i < chunksPerBlock_; ++i) {
+      current->next = reinterpret_cast<Chunk *>(start + i * chunkSize);
+      current = new (current->next) Chunk();
     }
+    current->next = nullptr;
+    return reinterpret_cast<Chunk *>(start);
+  }
 };
